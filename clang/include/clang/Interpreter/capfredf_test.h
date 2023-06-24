75f77f1463493a4f4da57b14336b524dd9efcba8
Fred Fu <moonsolo@gmail.com>
2023-06-24 19:09:53 +0000

    so long, farewell my friend!

--- clang/include/clang/Interpreter/capfredf_test.h
+++
@@ -1,8 +0,0 @@
-#ifndef CAPFREDF_TEST_H
-#define CAPFREDF_TEST_H
-#include <vector>
-#include "clang/Interpreter/Interpreter.h"
-void capfredf_test(std::vector<const char *> &ArgStrs, std::string FN, size_t Line, size_t Col);
-void capfredf_test2(clang::Interpreter &Interp, std::string FN, size_t Line, size_t Col);
-llvm::Expected<bool> capfredf_test3(clang::IncrementalCompilerBuilder& CB, std::string FN, size_t Line, size_t Col);
-#endif
