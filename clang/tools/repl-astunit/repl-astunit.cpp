8acff935e598f38caa3b5c136b53bd3af0e1cdc8
Fred Fu <moonsolo@gmail.com>
2023-08-11 15:11:36 -0400

    up

--- clang/tools/repl-astunit/repl-astunit.cpp
+++ clang/tools/repl-astunit/repl-astunit.cpp
@@ -1,9 +1,11 @@
 #include "clang/Basic/Diagnostic.h"
+#include "clang/Frontend/ASTUnit.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendDiagnostic.h"
 #include "clang/Interpreter/CodeCompletion.h"
 #include "clang/Interpreter/Interpreter.h"
 #include "clang/Sema/CodeCompleteConsumer.h"
+#include "clang/Serialization/PCHContainerOperations.h"
 
 #include "llvm/ExecutionEngine/Orc/LLJIT.h"
 #include "llvm/LineEditor/LineEditor.h"
@@ -11,6 +13,7 @@
 #include "llvm/Support/ManagedStatic.h" // llvm_shutdown
 #include "llvm/Support/Signals.h"
 #include "llvm/Support/TargetSelect.h"
+#include "llvm/ADT/SmallVector.h"
 
 #include <iostream>
 
@@ -33,8 +36,60 @@ int main(int argc, const char **argv) {
 
   clang::IncrementalCompilerBuilder CB;
 
-  std::unique_ptr<clang::CompilerInstance> CI = ExitOnErr(CB.CreateCpp());
+  // Perform the remapping of source files.
+  const char* complete_filename="/home/capfredf/tmp/hello1.cpp";
 
-  std::cout<< "Hello, WORLD\n";
+  llvm::SmallVector<clang::ASTUnit::RemappedFile, 4> RemappedFiles;
+
+  if (llvm::ErrorOr<std::unique_ptr<llvm::MemoryBuffer>> MB =
+      llvm::MemoryBuffer::getFileOrSTDIN(complete_filename, true); !MB.getError())
+
+    RemappedFiles.push_back(std::make_pair(complete_filename, MB->release()));
+
+  unsigned complete_line = 2, complete_column = 1;
+  std::vector<clang::CodeCompletionResult> CCResults;
+  std::unique_ptr<clang::CompilerInstance> CI = ExitOnErr(CB.CreateCpp());
+  auto DiagOpts = clang::DiagnosticOptions();
+  auto consumer = clang::ReplCompletionConsumer(CCResults);
+  std::unique_ptr<clang::Interpreter> Interp = ExitOnErr(clang::Interpreter::create(std::move(CI)));
+  auto* InterpCI = const_cast<clang::CompilerInstance*>(Interp->getCompilerInstance());
+  auto diag = InterpCI->getDiagnosticsPtr();
+  clang::ASTUnit* AU =
+    clang::ASTUnit::LoadFromCompilerInvocationAction(const_cast<clang::CompilerInstance*>(Interp->getCompilerInstance())->getInvocationPtr(),
+                                                     std::make_shared<clang::PCHContainerOperations>(),
+                                                     diag);
+  {
+    llvm::SmallVector<clang::StoredDiagnostic, 8> sd = {};
+    llvm::SmallVector<const llvm::MemoryBuffer *, 1> tb = {};
+    InterpCI->getFrontendOpts().Inputs[0] = clang::FrontendInputFile(complete_filename, clang::Language::CXX, clang::InputKind::Source);
+
+    AU->CodeComplete(complete_filename, complete_line, complete_column,
+                   RemappedFiles, false,
+                   false,
+                   false,
+                   consumer,
+                   std::make_shared<clang::PCHContainerOperations>(),
+                   *diag,
+                   InterpCI->getLangOpts(),
+                   InterpCI->getSourceManager(),
+                   InterpCI->getFileManager(),
+                   sd, tb);
+  }
+
+  for (auto Res : CCResults) {
+    switch (Res.Kind) {
+    case clang::CodeCompletionResult::RK_Declaration:
+      if (auto *ID = Res.Declaration->getIdentifier()) {
+        std::cout << " ID " << ID->getName().str() << "\n";
+      }
+      break;
+    case clang::CodeCompletionResult::RK_Keyword:
+      std::cout << " sym " << Res.Keyword << "\n";
+      break;
+    default:
+      break;
+    }
+  }
+
   return 0;
 }
